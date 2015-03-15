f75799ac0391427409be84533b3686cd6669d896
James Halliday <mail@substack.net>
2015-03-15 15:26:33 -0700

    using virtual-dom/virtual-hyperscript/svg

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
-var h = require('virtual-hyperscript');
+var ns = 'http://www.w3.org/2000/svg';
+var hsvg = require('virtual-dom/virtual-hyperscript/svg');
+var has = require('has');
 var isarray = require('isarray');
 var xtend = require('xtend');
 
-var ns = 'http://www.w3.org/2000/svg';
-
-module.exports = function (name, props, ch) {
-    if (name === 'svg' && (!props || !props.xmlns)) {
-        props = xtend({ xmlns: ns }, props);
+module.exports = function (name, props, children) {
+    if (name.toUpperCase() === 'SVG' && !has(props, 'xmlns')) {
+        if (typeof props === 'string') {
+            children = [ props ];
+            props = {};
+        }
+        else if (isarray(props)) {
+            children = props;
+            props = {};
+        }
+        return hsvg(name, xtend(props, { xmlns: ns }), children);
     }
-    if (props && !isarray(props)) {
-        return h(name, {
-            namespace: ns,
-            attributes: props
-        }, ch);
-    }
-    else return h(name, { namespace: ns }, props);
+    else return hsvg(name, props, children);
 };
